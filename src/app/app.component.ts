e6dbed0c66a033527ec029d405d05bd42e7ab4db
kherrera <kenher822@gmail.com>
2019-10-23 20:41:06 -0600

    Cambio en el título de la WebApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'instalacion-angular';
+  title = 'Kennett Herrera WebApp';
 }
