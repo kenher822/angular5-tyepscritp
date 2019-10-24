043ec27f7f3cfa91e701a2faceaf36711880e84c
kherrera <kenher822@gmail.com>
2019-10-23 21:53:14 -0600

    Agregando directiva de bucle for

--- src/app/videojuegos/videojuegos.components.ts
+++ src/app/videojuegos/videojuegos.components.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-    selector: 'videojuegos',
-    template: `
-        <h2>Componente de video juegos 2018</h2>
-        <h3 *ngIf="mostrar_retro != true">El mejor juego es: {{mejor_juego}}</h3>
-        <h3 [style.background]="color"
-            *ngIf="mostrar_retro">
-            El juego retro es: {{mejor_juego_retro}}
-        </h3>
-    `
+  selector: "videojuegos",
+  template: `
+    <h2>Componente de video juegos 2018</h2>
+    <h3 *ngIf="mostrar_retro != true">El mejor juego es: {{ mejor_juego }}</h3>
+    <h3 [style.background]="color" *ngIf="mostrar_retro">
+      El juego retro es: {{ mejor_juego_retro }}
+    </h3>
+    <h2>Listado de video juegos</h2>
+    <ul>
+        <li *ngFor="let game of videojuegos">{{game}}</li>
+    </ul>
+  `
 })
-export class VideoJuegosComponent{
-    public nombre = 'Videojuegos 2018';
-    public mejor_juego = 'GTA-5';
-    public mejor_juego_retro = 'Super Mario 64';
-    public mostrar_retro = true;
-    public color="violet";
+export class VideoJuegosComponent {
+  public nombre = "Videojuegos 2018";
+  public mejor_juego = "GTA-5";
+  public mejor_juego_retro = "Super Mario 64";
+  public mostrar_retro = true;
+  public color = "violet";
+
+  public videojuegos = [
+    "Los simpson hit and run",
+    "Assasins creed",
+    "GTA 5",
+    "Call of Duty",
+    "Tekken"
+  ];
 }
